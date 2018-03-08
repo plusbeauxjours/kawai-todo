10eb558283ad24904d4d7d41423819afb1f51699
plusbeauxjours <plusbeauxjours@gmail.com>
2018-03-08 23:39:10 +0900

    Coding the UI part Three

--- ToDo.js
+++ ToDo.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window')
 
 export default class ToDo extends Component{
+    state = {
+        isEditing: false
+    }
     render(){
         return (
-            <View>
-                <Text>Hello I'm A To Do</Text>
+            <View style={styles.container}>
+                <TouchableOpacity>
+                    <View style={styles.circle} />
+                </TouchableOpacity>
+                <Text style={styles.text}>Hello I'm A To Do</Text>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+    container: {
+        width: width - 50,
+        borderBottomColor: '#bbb',
+        borderBottomWidth: StyleSheet.hairlineWidth
+    },
+    text: {
+        fontWeight: '600',
+        fontSize: 20, 
+        marginVertical: 20
+    }
+});
